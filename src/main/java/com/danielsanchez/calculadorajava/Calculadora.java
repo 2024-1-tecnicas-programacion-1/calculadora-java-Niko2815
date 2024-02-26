08673076c00fd95a5db122c1611e7297af78b96d
jyma1204 <jyma1204@gmail.com>
2024-02-26 10:12:19 -0500

    Calculadora Java 1
    Se agrego la raiz

--- src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
+++ src/main/java/com/danielsanchez/calculadorajava/Calculadora.java
@@ -34,6 +34,10 @@ public class Calculadora {
                 // Operación división:
                 respuesta = (double) Math.pow(numero1,numero2);
                 break;
+            case 'r' :
+                // Operación raiz:
+                respuesta = (double) Math.pow(numero1,(double) 1/numero2);
+                break;
             // TODO: DEBES CREAR AQUÍ LOS CASES PARA LA OPERACIÓN DE LA POTENCIA Y EL MÓDULO.
                 
             default:
@@ -53,7 +57,7 @@ public class Calculadora {
         
         // Lectura de la operación a realizar
         // TODO: DEBES ADICIONAR EL SÍMBOLO DE ^ Y % PARA CALCULAR LA POTENCIA Y EL MÓDULO
-        System.out.println("Ingrese la operación (+, -, *, /, ^, %)");
+        System.out.println("Ingrese la operación (+, -, *, /, ^, %, r)");
         char operacion = lector.next().charAt(0);
         
         double respuesta = calcular(numero1, numero2, operacion);
